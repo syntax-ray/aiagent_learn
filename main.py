319a342b6748f7d1b92c62ca20625a99444a25e9
Donald Ray <syntaxray@gmail.com>
2025-07-22 17:54:19 +0100

    Add verbose flag

--- main.py
+++ main.py
@@ -11,21 +11,27 @@ FREE_TEIR_MODEL = 'gemini-2.0-flash-001'
 cli_arguements = sys.argv
 
 def main():
+    verbose = False
     if len(cli_arguements) == 1:
         print('No prompt was provided')
         exit(1)
     else:
+        if cli_arguements and len(cli_arguements) >= 3:
+            if cli_arguements[2] == '--verbose':
+                verbose = True
         user_prompt = cli_arguements[1]
         messages = [
             types.Content(role='user', parts=[types.Part(text=user_prompt)])
         ]
         print("Hello from aiagent!")
-        print(f'The prompt entered is {user_prompt}')
+        if verbose:
+            print(f'The prompt entered is:::: {user_prompt}')
         ai_response = client.models.generate_content(model=FREE_TEIR_MODEL,
                                                     contents=messages)
-        print(f'The ai agent says::::\n{ai_response.text}')
-        print(f'Prompt tokens: {ai_response.usage_metadata.prompt_token_count}')
-        print(f'Response tokens: {ai_response.usage_metadata.candidates_token_count}')
+        print(f'The ai agent says:::: {ai_response.text}')
+        if verbose:
+            print(f'Prompt tokens: {ai_response.usage_metadata.prompt_token_count}')
+            print(f'Response tokens: {ai_response.usage_metadata.candidates_token_count}')
 
 
 if __name__ == "__main__":
